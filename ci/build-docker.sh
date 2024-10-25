9de410826f764413e86d70f50af111483a408996
Andrey Todorov <97359396+Stelllarce@users.noreply.github.com>
2024-10-25 16:48:08 +0300

    Update build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-Tag="$(git rev-parse --short "$GITHUB_SHA")" 
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
 REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
 docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
